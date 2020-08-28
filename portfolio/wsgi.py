acb07d1229a832f42dce98f7b9f1b82f44d3e76e
Saimon <saimonribeiros@hotmail.com>
2020-08-28 09:08:52 -0300

    Update wsgi.py

--- portfolio/wsgi.py
+++ portfolio/wsgi.py
@@ -2,5 +2,5 @@ import os
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings.dev')
 application = Cling(get_wsgi_application())
