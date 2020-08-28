edd0d4e7be6b1728ccf5e8d41fc7180f92a58f72
Saimon <saimonribeiros@hotmail.com>
2020-08-28 11:08:12 -0300

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -65,7 +65,7 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default':{
-        'ENGINE':'django.db.backends.sqlite3'
+        'ENGINE':'django.db.backends.sqlite3',
         'NAME':os.path.join(BASE_DIR,'db.sqlite3'),
     }
 
